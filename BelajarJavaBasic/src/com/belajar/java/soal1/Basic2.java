5ce305b477c11573bdf9c23d9fbb14b2c611a22e
ahmadfeisalnh <38941610+ahmadfeisalnh@users.noreply.github.com>
2018-05-13 13:36:14 +0700

    Update Basic2.java

--- BelajarJavaBasic/src/com/belajar/java/soal1/Basic2.java
+++ BelajarJavaBasic/src/com/belajar/java/soal1/Basic2.java
@@ -9,7 +9,7 @@ public class Basic2 {
   private void soal2(int n) {
 	  for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (i+j>=n-1) {
+			if (i+j==n-1) {
 				System.out.print("*\t");
 			}else {
 				System.out.print("\t");
